70b5c0d72baf410394118c6404f9e7eb2a5f6343
kdw <ksgkdw78@naver.com>
2025-02-18 17:25:29 +0900

    stockpin back init

---
+++ src/main/java/com/stockpin/project/StockpinApplication.java
@@ -0,0 +1,13 @@
+package com.stockpin.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockpinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockpinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stockpin/project/StockpinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stockpin.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockpinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
